aaf456263e3bb9b84bd483a3677bab4541760a66
Karim Ančić <karim.ancic@asseco-see.hr>
2018-11-03 19:58:03 +0100

    Use LiveField

--- app/src/main/java/ancic/karim/gitbro/ui/main/MainViewModel.kt
+++ app/src/main/java/ancic/karim/gitbro/ui/main/MainViewModel.kt
@@ -6,14 +6,12 @@ import ancic.karim.gitbro.ui.base.BaseRouter
 import ancic.karim.gitbro.ui.base.BaseViewModel
 import android.app.Application
 import androidx.arch.core.util.Function
-import androidx.databinding.ObservableField
 import androidx.databinding.library.baseAdapters.BR
-import androidx.lifecycle.MutableLiveData
 
 class MainViewModel(application: Application) : BaseViewModel<MainRepository, BaseRouter>(application, MainRepository(), BaseRouter()) {
-    val repositoryVariableId = ObservableField<Int?>(BR.repository)
-    val searchText = MutableLiveData<String?>()
-    val sortText = MutableLiveData<String?>()
+    val repositoryVariableId = LiveField<Int?>(BR.repository)
+    val searchText = LiveField<String?>()
+    val sortText = LiveField<String?>()
     val repositoryList = LiveField<List<Repository>>(searchText, sortText, switchMapFunction = Function {
         repository.getRepositoryList(searchText.value, sortText.value)
     })
