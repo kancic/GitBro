5cc8f54fed932208a94467a0a896b5972e33b5c1
Karim Ančić <karim.ancic@asseco-see.hr>
2018-11-08 00:33:33 +0100

    Use toolbar instead of action bar

--- app/src/main/java/ancic/karim/gitbro/ui/base/BaseActivity.kt
+++ app/src/main/java/ancic/karim/gitbro/ui/base/BaseActivity.kt
@@ -1,22 +1,31 @@
 package ancic.karim.gitbro.ui.base
 
 import ancic.karim.gitbro.BR
+import ancic.karim.gitbro.R
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 @SuppressLint("Registered")
 abstract class BaseActivity<BINDING : ViewDataBinding, VIEW_MODEL : BaseViewModel<*, *>> : AppCompatActivity() {
     protected val activity: AppCompatActivity by lazy { this }
     protected lateinit var binding: BINDING
     protected lateinit var viewModel: VIEW_MODEL
+    protected lateinit var navigationController: NavController
 
     protected abstract fun provideViewResourceId(): Int
     protected abstract fun provideViewModelClass(): Class<VIEW_MODEL>
+    protected abstract fun provideToolbar(): Toolbar?
+    protected open fun provideNavigationHostFragmentId() = 0
     protected open fun provideOptionsMenuResourceId() = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +38,18 @@ abstract class BaseActivity<BINDING : ViewDataBinding, VIEW_MODEL : BaseViewMode
             binding = DataBindingUtil.setContentView(activity, provideViewResourceId())
             binding.setVariable(BR.viewModel, viewModel)
             binding.setLifecycleOwner(activity)
+        }
+
+        provideToolbar()?.also { toolbar ->
+            setSupportActionBar(toolbar)
+        }
+
+        if (provideNavigationHostFragmentId() != 0) {
+            navigationController = findNavController(provideNavigationHostFragmentId())
+            provideToolbar()?.apply {
+                val appBarConfiguration = AppBarConfiguration(navigationController.graph)
+                setupWithNavController(navigationController, appBarConfiguration)
+            }
         }
 
         intent?.extras?.also { extras ->
--- app/src/main/java/ancic/karim/gitbro/ui/main/MainActivity.kt
+++ app/src/main/java/ancic/karim/gitbro/ui/main/MainActivity.kt
@@ -1,13 +1,17 @@
 package ancic.karim.gitbro.ui.main
 
 import ancic.karim.gitbro.R
+import ancic.karim.gitbro.databinding.ActivityMainBinding
 import ancic.karim.gitbro.databinding.item.OnItemClickListener
 import ancic.karim.gitbro.ui.base.BaseActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.ViewDataBinding
 
-class MainActivity : BaseActivity<ViewDataBinding, MainViewModel>() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override fun provideViewResourceId() = R.layout.activity_main
     override fun provideViewModelClass() = MainViewModel::class.java
+    override fun provideToolbar() = binding.toolbar
+    override fun provideNavigationHostFragmentId() = R.id.navigation_host
 }
